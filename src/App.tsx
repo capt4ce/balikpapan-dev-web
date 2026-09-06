b6faf1095cd7e709ed2508e873c57c9b10143b54
AliAbdilah123 <ahmad.ali.abdillah@gmail.com>
2026-09-06 15:40:33 +0000

    Configure deployment under balikpapan-dev subpath

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/projects/balikpapan-dev">
           <Routes>
             <Route path="/" element={<Layout />}>
               <Route index element={<Home />} />
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import mdx from "@mdx-js/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/projects/balikpapan-dev/",
   server: {
     host: "::",
     port: 8080,
